0378918447223a0a6c69c577f61f1b13512e8e5b
Samuele Battaglino <s212245@studenti.polito.it>
2017-11-29 18:02:08 +0100

    Prova

---
+++ OMA/src/t.c
@@ -0,0 +1,14 @@
+/*
+ * t.c
+ *
+ *  Created on: 21 nov 2017
+ *      Author: Nicola
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("La mamma di Nicola\n"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
--- OMA/t.c
+++
@@ -1,8 +0,0 @@
-/*
- * t.c
- *
- *  Created on: 21 nov 2017
- *      Author: Nicola
- */
-
-
