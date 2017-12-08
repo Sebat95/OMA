04a228db33ef40aa8362304c9569a37c4a2e1b62
Admin <Admin@Admin-PC.homenet.telecomitalia.it>
2017-12-08 21:54:55 +0100

    Buonanotte un cazzo

---
+++ OMA/src/OMA_libraries/logging.h
@@ -0,0 +1,16 @@
+/*
+ * logging.h
+ *
+ *  Created on: 08 dic 2017
+ *      Author: Samuele
+ */
+
+#ifndef SRC_OMA_LIBRARIES_LOGGING_H_
+#define SRC_OMA_LIBRARIES_LOGGING_H_
+
+void start_timer();
+void stop_timer();
+void logger(char **things, int n);
+void instant_wb(char **things, int n);
+
+#endif /* SRC_OMA_LIBRARIES_LOGGING_H_ */
