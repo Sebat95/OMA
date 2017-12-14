acbd08895989c1ac66aa25bfe9defb02c2390279
Samuele Battaglino <s212245@studenti.polito.it>
2017-12-14 22:48:43 +0100

    Method3 WORKING

--- OMA/src/OMA_libraries/method3.c
+++ OMA/src/OMA_libraries/method3.c
@@ -7,13 +7,46 @@
 #define DEBUG_METHOD3
 
 #include "method1.h"
+#include "method3.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h> //per srand
 #include <string.h> //for memcpy
 #include <math.h>
-#define STOP 1000
 
-static double penalty(int *x, int T, int E, int S, int **n){
+#define STOP 10e10
+#define TOP 10e10
+#define ESC1 100
+#define ESC2 1000
+#define OUT 50
+
+
+/*static double penalty_ex(int *x, int T, int E, int S, int **n, pen_per_ex *p){ //compute penalty
+	int i, j, delta;
+	double pen=0.0, adder;
+
+	for(i=0; i<T;i++){ //reinitialize pen_ts
+		p[i].pen=0.0;
+		p[i].ex=0;
+	}
+
+	for(i=0; i<E; i++)
+		for(j=i+1; j<E; j++){
+			delta=abs(x[i]-x[j]);
+			if(delta==0 && n[i][j]!=0) //unfeasable solution
+				return -1;
+			if(delta<=5 && delta >=0 && n[i][j]!=0){
+				adder=pow(2, 5-delta)*n[i][j]/S;
+				pen+=adder;
+				p[i].ex=i;
+				p[i].pen+=adder;
+			}
+		}
+
+	return pen;
+}*/
+
+static double penalty(int *x, int T, int E, int S, int **n){ //compute penalty
 	int i, j, delta;
 	double pen=0.0;
 
@@ -23,33 +56,57 @@ static double penalty(int *x, int T, int E, int S, int **n){
 			if(delta==0 && n[i][j]!=0) //unfeasable solution
 				return -1;
 			if(delta<=5 && delta >=0 && n[i][j]!=0)
-				pen+=pow(2, delta)*n[i][j]/S;
+				pen+=pow(2, 5-delta)*n[i][j]/S;;
 		}
 
 	return pen;
 }
 
-static void swap(int *x, int init, int end){
-	int i, app, factor = ceil(((double)end - (double)init)/3.0);
+static void swap_ts(int *x, int E, int exch, int t){ //swap the chosen timeslots t and exch
+	int i;
 
-	for(i=init; i<(factor+init); i++){
-		app=x[i];
-		x[i]=x[end-i+init-1];
-		x[end-i+init-1]=app;
+	for(i=0; i<E; i++){
+		if (x[i]==t){
+			x[i]=exch;
+			continue;
+		}
+		if (x[i]==exch){
+			x[i]=t;
+			continue;
+		}
 	}
 }
 
-static double min(int a, int b){
-	if(a<=b)
-		return a;
-	else
-		return b;
-}
+/*int ppt_desc(const void* a, const void* b) //function to be passed to qsort for ordering pen_per_ts decreasingly
+{
+	pen_per_ex A = *(pen_per_ex*)a, B = *(pen_per_ex*) b;
+	double x = B.pen - A.pen;
+	if(x>0)
+		return 1;
+	if(x<0)
+		return -1;
+	return 0;
+}*/
 
 void optimizationMethod3(int *x, int T, int E, int S, int **n, int *students_per_exam, int **conflictual_students, char *instance_name)
 {
-	int iter=0, mul=0, begin=0, end=E, best[E], step=E;
-	double temp=1.0, new_pen, best_pen=penalty(x, T, E, S, n);
+	int iter, best[E], i, exch1, exch2, outer=0, cnt=0;
+	double best_pen, new_pen, temp=1.0;
+
+	srand(time(NULL)); //rand initialization
+
+	best_pen=penalty(x, T, E, S, n); //best_pen & pen_best init
+
+	//qsort(pen_best, T, sizeof(pen_per_ts), ppt_desc); //sorting pen_ts in decreascing order
+
+	/*memcpy(pen_new, pen_best, T*sizeof(pen_per_ts)); //pen_new init
+
+	#ifdef DEBUG_METHOD3
+	printf("\nTime slot ordered per penalty:\n");
+	for(i=0; i<T; i++){
+		printf( "x[%d] = %f\n", pen_best[i].ts, pen_best[i].pen);
+	}
+	#endif*/
 
 	memcpy(best, x, E*sizeof(int)); //initialize best solution
 
@@ -57,44 +114,101 @@ void optimizationMethod3(int *x, int T, int E, int S, int **n, int *students_per
 	printf("\nInitial penalty: %f\n", best_pen);
 	#endif
 
+	while(outer<OUT){
+		iter=0;
+		cnt=0;
+		temp-=temp/outer;
 	while(iter<STOP){ //stopping condition??
-		//prob = (double)rand() / (double)RAND_MAX; //probability to make or not the swap
-		//if ((prob+temp)/2 > 0.5){
-		if(temp){
-			swap(x, begin, end);
-			if(end>E-step){
-				mul++;
-				step = min((int) (end/(mul*3)), step-1);
-				if(step<3)
-					break;
-				end = step;
-				begin=0;
-			}
-			else{
-				begin = end;
-				end += step;
-			}
-			new_pen = penalty(x, T, E, S, n);
-			#ifdef DEBUG_METHOD3
-			printf("New Penalty %d - %d: %f; Best penalty: %f\n", begin-step, begin ,new_pen, best_pen);
-			#endif
-			if (new_pen <= best_pen && new_pen!=-1) {
-				memcpy(best, x, E*sizeof(int));
-				best_pen=new_pen;
-			}
-			//else
-				//memcpy(x, best, E*sizeof(int)); //restore the better one
+		exch1 = rand() % T; //swap two random timeslot
+		exch2 = exch1;
+		//prob= rand() / RAND_MAX;
+		while(exch2 == exch1) //different from each others
+			exch2 = rand() % T;
+		swap_ts(x, E, exch2, exch1);
+		new_pen = penalty(x, T, E, S, n); //compute new results
+   		#ifdef DEBUG_METHOD3
+		printf("New Penalty: %f; Best penalty: %f\n", new_pen, best_pen);
+		#endif
+		if (new_pen < best_pen && new_pen!=-1) { //if it's an improvement update "best" variables
+			memcpy(best, x, E*sizeof(int));
+			best_pen=new_pen;
+			cnt=0;
+		}
+		else{ //if it wasn't an improvement
+			//if(new_pen==-1)
+				memcpy(x, best, E*sizeof(int)); //restore the best one
+			/*if(temp<prob)
+				memcpy(x, best, E*sizeof(int)); //restore the best one*/
+			if(++cnt==ESC1)
+				break;
 		}
 		iter++; //count the iteration
-		//temp= temp - iter/STOP; //decrease the temperature according to the iteration number
 	}
+
 	#ifdef DEBUG_METHOD3
-	int i;
 	printf("\n\n\nImproved penalty: %f\n", best_pen);
 	printf("Improved solution:\n");
 	for(i=0; i<E; i++){
 		printf( "x[%d] = %d\n", i+1, best[i]);
 	}
 	#endif
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/*best_pen=penalty_ex(x, T, E, S, n, pen_best); //best_pen & pen_best init
+
+	qsort(pen_best, E, sizeof(pen_per_ex), ppt_desc); //sorting pen_ts in decreascing order
+
+	memcpy(pen_new, pen_best, E*sizeof(pen_per_ex)); //pen_new init
+
+	#ifdef DEBUG_METHOD3
+	printf("\nExam ordered per penalty:\n");
+	for(i=0; i<E; i++){
+		printf( "%d = %f\n", pen_best[i].ex, pen_best[i].pen);
+	}
+	#endif*/
+
+	cnt=0;
+	iter=0;
+	while(iter<TOP){ //stopping condition??
+		exch1 = rand() % E; //swap the one of the TOP worst timeslot with exchange
+		exch2=x[exch1];
+		//prob= rand() / RAND_MAX;
+		for(i=0; i<T; i++){
+			x[exch1]=i;
+			new_pen = penalty(x, T, E, S, n); //compute new results
+			#ifdef DEBUG_METHOD3
+			printf("New Penalty: %f; Best penalty: %f\n", new_pen, best_pen);
+			#endif
+			if (new_pen < best_pen && new_pen!=-1) { //if it's an improvement update "best" variables
+				best[exch1]=i;
+				/*qsort(pen_new, E, sizeof(pen_per_ex), ppt_desc); //sorting pen_ts in decreascing order
+				memcpy(pen_best, pen_new, E*sizeof(pen_per_ex)); //pen_new init*/
+				best_pen=new_pen;
+				cnt=0;
+			}
+			else{ //if it wasn't an improvement
+				if(new_pen==-1)
+					x[exch1]=exch2;
+				if(++cnt==ESC2)
+					break;
+			}
+		}
+		if(cnt==ESC2)
+			break;
+		iter++; //count the iteration
+	}
+
+	#ifdef DEBUG_METHOD3
+	printf("\n\n\nImproved penalty: %f\n", best_pen);
+	printf("Improved solution:\n");
+	for(i=0; i<E; i++){
+		printf( "x[%d] = %d\n", i+1, best[i]);
+	}
+	#endif
+
+	outer++;
+	}
+
+	//free(pen_best);
+	//free(pen_new);
 }
 
