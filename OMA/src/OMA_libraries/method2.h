74277212c28120054ceb8f15ccc2680fe4cc4743
Nicola <Nicola@DESKTOP-0LSRTB9>
2017-12-17 17:41:38 +0100

    g

---
+++ OMA/src/OMA_libraries/method2.h
@@ -0,0 +1,17 @@
+/*
+ * method2.h
+ *
+ *  Created on: 02 dic 2017
+ *      Author: Nicola
+ *
+ *
+ *      METHOD DESCRIPTION:
+ *      ...
+ */
+
+#ifndef SRC_OMA_LIBRARIES_METHOD2_H_
+#define SRC_OMA_LIBRARIES_METHOD2_H_
+
+void optimizationMethod2(int *x, int T, int E, int S, int **n, int *students_per_exam, char *instance_name);
+
+#endif /* SRC_OMA_LIBRARIES_METHOD1_H_ */
